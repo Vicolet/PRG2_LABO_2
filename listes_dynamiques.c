49d4f1326e48b97442041a546d1698a867c1f88c
Fleury-Romain <romain.fleury@heig-vd.ch>
2023-04-26 19:45:00 +0200

    Inserer début et fin

--- listes_dynamiques.c
+++ listes_dynamiques.c
@@ -13,7 +13,6 @@
  -----------------------------------------------------------------------------------
 */
 #include <stdlib.h>
-#include <malloc.h>
 #include "listes_dynamiques.h"
 
 Liste* initialiser(void){
@@ -23,7 +22,11 @@ Liste* initialiser(void){
 }
 
 bool estVide(const Liste* liste){
-   if()
+   if(liste->tete == NULL && liste->queue == NULL){
+      return true;
+   }
+
+   return false;
 }
 
 size_t longueur(const Liste* liste){
@@ -35,11 +38,55 @@ void afficher(const Liste* liste, Mode mode){
 }
 
 Status insererEnTete(Liste* liste, const Info* info){
+   // vérifier si la liste est vide
+   // si liste vide calloc un "Elements" et tete = calloc
 
+   // si liste non vide, calloc un "Elements" et tmp = tete, tete = calloc, tete
+   // .suivant = tmp
+
+   Element* eptr = (Element*) calloc(1, sizeof(Element));
+   if(eptr != NULL) {
+      if (estVide(liste)) {
+         liste->tete = eptr;
+         liste->queue = eptr;
+         liste->tete->info = *info;
+
+         return OK;
+      } else {
+         Element *tmp = liste->tete;
+         liste->tete = eptr;
+         liste->tete->suivant = tmp;
+         liste->tete->suivant->precedent = liste->tete;
+         liste->tete->info = *info;
+
+         return OK;
+      }
+   }else{
+      return MEMOIRE_INSUFFISANTE;
+   }
 }
 
 Status insererEnQueue(Liste* liste, const Info* info){
+   Element* eptr = (Element*) calloc(1, sizeof(Element));
+   if(eptr != NULL){
+      if(estVide(liste)){
+         liste->tete = eptr;
+         liste->queue = eptr;
+         liste->queue->info = *info;
 
+         return OK;
+      }else{
+         Element* tmp = liste->queue;
+         liste->queue = eptr;
+         liste->queue->precedent = tmp;
+         liste->queue->precedent->suivant = liste->queue;
+         liste->queue->info = *info;
+
+         return OK;
+      }
+   }else{
+      return MEMOIRE_INSUFFISANTE;
+   }
 }
 
 Status supprimerEnTete(Liste* liste, Info* info){
--- main.c
+++ main.c
@@ -16,11 +16,47 @@
 #include "listes_dynamiques.h"
 
 int main() {
+   // Initialiser
    Liste* lptr = initialiser();
 
    printf("lptr : 0x%p \n", lptr);
    printf("lptr.tete : 0x%p \n", lptr->tete);
    printf("lptr.queue : 0x%p \n", lptr->queue);
+
+   printf("Liste est vide : %d \n", estVide(lptr));
+
+   // Insérer en tête
+   const Info i = 7;
+   insererEnTete(lptr, &i);
+
+   printf("Element de tete : 0x%p , Valeur : %d\n", lptr->tete, lptr->tete->info);
+   printf("Element suivant : 0x%p \n", lptr->tete->suivant);
+   printf("Element precedent : 0x%p \n", lptr->tete->suivant);
+   printf("Liste est vide : %d \n", estVide(lptr));
+
+   const Info j = 6;
+   insererEnTete(lptr, &j);
+
+   printf("Element de tete : 0x%p , Valeur : %d\n", lptr->tete, lptr->tete->info);
+   printf("Element suivant : 0x%p , Valeur : %d \n", lptr->tete->suivant,
+          lptr->tete->suivant->info);
+   printf("Element precedent : 0x%p \n", lptr->tete->precedent);
+   printf("Liste est vide : %d \n", estVide(lptr));
+
+   const Info y = 2;
+   insererEnTete(lptr, &y);
+
+   printf("Element de tete : 0x%p , Valeur : %d\n", lptr->tete, lptr->tete->info);
+   printf("Element suivant : 0x%p , Valeur : %d \n", lptr->tete->suivant,
+          lptr->tete->suivant->info);
+   printf("Element precedent : 0x%p \n", lptr->tete->precedent);
+   printf("suive prec : 0x%p, %d\n", lptr->tete->suivant->precedent,
+          lptr->tete->suivant->precedent->info);
+   printf("suiv : 0x%p, %d\n", lptr->tete->suivant, lptr->tete->suivant->info);
+   printf("suive suiv : 0x%p, %d\n", lptr->tete->suivant->suivant,
+          lptr->tete->suivant->suivant->info);
+   printf("Queue : 0x%p, %d\n", lptr->queue, lptr->queue->info);
+   printf("Liste est vide : %d \n", estVide(lptr));
 
    return 0;
 }

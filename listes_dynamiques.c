b87ea3e7a21f8d1adb69d656e44310520aa1cb2e
Fleury-Romain <romain.fleury@heig-vd.ch>
2023-04-26 23:36:10 +0200

    Librairie Finie

--- listes_dynamiques.c
+++ listes_dynamiques.c
@@ -13,12 +13,13 @@
  -----------------------------------------------------------------------------------
 */
 #include <stdlib.h>
+#include <stdio.h>
 #include "listes_dynamiques.h"
 
 Liste* initialiser(void){
-   Liste* l = (Liste*) calloc(1, sizeof(Liste));
+   Liste* lptr = (Liste*) calloc(1, sizeof(Liste));
 
-   return l;
+   return lptr;
 }
 
 bool estVide(const Liste* liste){
@@ -48,7 +49,29 @@ size_t longueur(const Liste* liste){
 }
 
 void afficher(const Liste* liste, Mode mode){
-
+   if(estVide(liste)) {
+      printf("[]\n");
+   }else{
+      if (mode) { // forward
+         Element *eptr = liste->tete;
+         printf("[");
+         for (size_t i = 0; i < longueur(liste); i++) {
+            printf("%d", eptr->info);
+            if(i != longueur(liste)-1){printf(",");}
+            eptr = eptr->suivant;
+         }
+         printf("]\n");
+      } else { // backward
+         Element *eptr = liste->queue;
+         printf("[");
+         for (size_t i = 0; i < longueur(liste); i++) {
+            printf("%d", eptr->info);
+            if(i != longueur(liste)-1){printf(",");}
+            eptr = eptr->precedent;
+         }
+         printf("]\n");
+      }
+   }
 }
 
 Status insererEnTete(Liste* liste, const Info* info){
@@ -101,23 +124,87 @@ Status supprimerEnTete(Liste* liste, Info* info){
    if(estVide(liste)){
       return LISTE_VIDE;
    }else{
-
+      if(longueur(liste) == 1){
+         free(liste->tete);
+         liste->tete = NULL;
+         liste->queue = NULL;
+      }else{
+         Element* eptr = liste->tete;
+         *info = eptr->info;
+         liste->tete = liste->tete->suivant;
+         free(eptr);
+         liste->tete->precedent = NULL;
+      }
    }
 }
 
 Status supprimerEnQueue(Liste* liste, Info* info){
-
+   if(estVide(liste)){
+      return LISTE_VIDE;
+   }else{
+      if(longueur(liste) == 1){
+         free(liste->queue);
+         liste->tete = NULL;
+         liste->queue = NULL;
+      }else{
+         Element* eptr = liste->queue;
+         *info = liste->queue->info;
+         liste->queue = liste->queue->precedent;
+         free(eptr);
+         liste->queue->suivant = NULL;
+      }
+   }
 }
 
 void supprimerSelonCritere(Liste* liste,
                            bool (*critere)(size_t position, const Info* info)){
-
+   if(estVide(liste)){
+      return;
+   }else{
+      Element* eptr = liste->tete;
+      Info x;
+      for(size_t i = 0; i < longueur(liste); i++){
+         if(critere(i, &eptr->info)){
+            if(i == 0){ // effacer le premier
+               supprimerEnTete(liste, &x);
+            }else if(i == longueur(liste)){ // effacer le dernier
+               supprimerEnQueue(liste, &x);
+            }else{ // Effacer au milieu
+               Element* tmp = eptr;
+               eptr->suivant->precedent = eptr->precedent;
+               eptr->precedent->suivant = eptr->suivant;
+               free(tmp);
+               tmp = NULL;
+            }
+         }
+         eptr = eptr->suivant;
+      }
+   }
 }
 
 void vider(Liste* liste, size_t position){
-
+   if(position >= longueur(liste)){
+      return;
+   }else{
+      Element* eptr = liste->tete;
+      Info x;
+      for(size_t i = 0; i < position; i++){ eptr = eptr->suivant;}
+      while(liste->queue != eptr->precedent){
+         supprimerEnQueue(liste, &x);
+      }
+   }
 }
 
 bool sontEgales(const Liste* liste1, const Liste* liste2){
-
+   if(longueur(liste1) == longueur(liste2)){
+      Element *ptr1 = liste1->tete, *ptr2 = liste2->tete;
+      for(size_t i = 0; i < longueur(liste1); i++){
+         if(ptr1->info != ptr2->info){
+            return false;
+         }
+      }
+      return true;
+   }else{
+      return false;
+   }
 }
--- main.c
+++ main.c
@@ -15,6 +15,13 @@
 #include <stdio.h>
 #include "listes_dynamiques.h"
 
+bool critere(size_t pos, const Info *n){
+   if(pos % 2 == 0 || *n > 1 && *n < 6){
+      return true;
+   }
+   return false;
+}
+
 int main() {
    // Initialiser
    Liste* lptr = initialiser();
@@ -47,7 +54,7 @@ int main() {
    printf("Taille de la liste : %d\n", longueur(lptr));
 
    const Info y = 2;
-   insererEnTete(lptr, &y);
+   insererEnQueue(lptr, &y);
 
    printf("Element de tete : 0x%p , Valeur : %d\n", lptr->tete, lptr->tete->info);
    printf("Element suivant : 0x%p , Valeur : %d \n", lptr->tete->suivant,
@@ -61,6 +68,75 @@ int main() {
    printf("Queue : 0x%p, %d\n", lptr->queue, lptr->queue->info);
    printf("Liste est vide : %d \n", estVide(lptr));
    printf("Taille de la liste : %d\n", longueur(lptr));
+
+   // Supprimer tete
+   Info x;
+   supprimerEnTete(lptr, &x);
+   printf("\nAncien n : %d\n", x);
+   printf("Prec: 0x%p\n", lptr->tete->precedent);
+   printf("Tete: 0x%p, %d\n", lptr->tete, lptr->tete->info);
+   printf("Suiv: 0x%p, %d\n", lptr->tete->suivant, lptr->tete->suivant->info);
+   printf("Taille de la liste : %d\n", longueur(lptr));
+
+   // Supprimer queue
+   supprimerEnQueue(lptr, &x);
+   printf("\nPrec: 0x%p\n", lptr->tete->precedent);
+   printf("Tete: 0x%p, %d\n", lptr->tete, lptr->tete->info);
+   printf("Suiv: 0x%p\n", lptr->tete->suivant);
+   printf("Taille de la liste : %d\n", longueur(lptr));
+
+   // Vider
+   insererEnTete(lptr, &i);
+   insererEnTete(lptr, &j);
+   insererEnTete(lptr, &y);
+   insererEnTete(lptr, &j);
+   insererEnTete(lptr, &y);
+
+   /*
+   Element* eptr = lptr->tete->suivant->suivant;
+   printf("\n3eme: 0x%p\n", eptr);
+   printf("queue: 0x%p\n", lptr->queue);
+   supprimerEnQueue(lptr,&x);
+   printf("queue: 0x%p\n", lptr->queue);
+   supprimerEnQueue(lptr,&x);
+   printf("queue: 0x%p\n", lptr->queue);
+   supprimerEnQueue(lptr,&x);
+   printf("queue: 0x%p\n", lptr->queue);
+   */
+
+   printf("\ntaille: %d\n", longueur(lptr));
+   vider(lptr, 0);
+   printf("\ntaille: %d\n", longueur(lptr));
+
+   // égalité
+   Liste* l2ptr = initialiser();
+   Liste* l3ptr = initialiser();
+   insererEnTete(l2ptr, &i);
+   insererEnTete(l2ptr, &j);
+   insererEnTete(l2ptr, &y);
+   insererEnTete(l2ptr, &j);
+   insererEnTete(l2ptr, &y);
+
+   insererEnTete(l3ptr, &i);
+   insererEnTete(l3ptr, &j);
+   insererEnTete(l3ptr, &y);
+   insererEnTete(l3ptr, &j);
+   insererEnTete(l3ptr, &y);
+
+   printf("\nEgale: %d\n", sontEgales(l2ptr, l3ptr));
+   supprimerEnTete(l2ptr, &x);
+   printf("Egale: %d\n", sontEgales(l2ptr, l3ptr));
+   insererEnTete(l2ptr, &y);
+   printf("Egale: %d\n", sontEgales(l2ptr, l3ptr));
+
+   // Affichage
+   printf("\n");
+   afficher(l2ptr, FORWARD);
+   afficher(l2ptr, BACKWARD);
+
+   // SupprimerCritère
+   supprimerSelonCritere(l2ptr, critere);
+   afficher(l2ptr, FORWARD);
 
    return 0;
 }
